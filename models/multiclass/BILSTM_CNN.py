0b76b3e4cc4a3b634a45560d37ac15cac6eb5376
sori424 <47997074+sori424@users.noreply.github.com>
2021-04-15 10:19:46 +0900

    Update BILSTM_CNN.py

--- models/multiclass/BILSTM_CNN.py
+++ models/multiclass/BILSTM_CNN.py
@@ -1,5 +1,5 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np 
+import pandas as pd 
 import keras
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
@@ -11,13 +11,11 @@ from keras.models import Model
 from keras.callbacks import EarlyStopping
 
 
-
 import tensorflow as tf
 if tf.test.gpu_device_name():
     print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
 else:
     print("Please install GPU version of TF")
-
 
 
 train = pd.read_csv('train.csv')
@@ -27,17 +25,13 @@ train.type = train.type-1
 test.type = test.type-1
 
 
-
 x_train = train['tokenized'].values
 y_train = train['type'].values
 x_test= test['tokenized'].values
 y_test = test['type'].values
 
-
-
 tokenizer = Tokenizer()
 tokenizer.fit_on_texts(x_train)
-
 
 threshold = 2
 total_cnt = len(tokenizer.word_index) 
@@ -52,23 +46,18 @@ for key, value in tokenizer.word_counts.items():
         rare_cnt = rare_cnt + 1
         rare_freq = rare_freq + value
 
-
-
-
 vocab_size = total_cnt - rare_cnt + 2
 
 
 threshold = 2
-total_cnt = len(tokenizer.word_index) # 단어의 수
-rare_cnt = 0 # 등장 빈도수가 threshold보다 작은 단어의 개수를 카운트
-total_freq = 0 # 훈련 데이터의 전체 단어 빈도수 총 합
-rare_freq = 0 # 등장 빈도수가 threshold보다 작은 단어의 등장 빈도수의 총 합
+total_cnt = len(tokenizer.word_index) 
+rare_cnt = 0 
+total_freq = 0 
+rare_freq = 0 
 
-# 단어와 빈도수의 쌍(pair)을 key와 value로 받는다.
 for key, value in tokenizer.word_counts.items():
     total_freq = total_freq + value
 
-    # 단어의 등장 빈도수가 threshold보다 작으면
     if(value < threshold):
         rare_cnt = rare_cnt + 1
         rare_freq = rare_freq + value
@@ -78,12 +67,8 @@ print('등장 빈도가 %s번 이하인 희귀 단어의 수: %s'%(threshold - 1
 print("단어 집합에서 희귀 단어의 비율:", (rare_cnt / total_cnt)*100)
 print("전체 등장 빈도에서 희귀 단어 등장 빈도 비율:", (rare_freq / total_freq)*100)
 
-
-
 print('최대 길이 :',max(len(l) for l in x_train))
 print('평균 길이 :',sum(map(len, x_train))/len(x_train))
-
-
 
 tokenizer = Tokenizer(vocab_size, oov_token = 'OOV') 
 tokenizer.fit_on_texts(x_train)
@@ -91,15 +76,9 @@ x_train = tokenizer.texts_to_sequences(x_train)
 x_test = tokenizer.texts_to_sequences(x_test)
 
 
-
 max_len = 58
-
-
-
 x_train = pad_sequences(x_train, maxlen = max_len)
 x_test = pad_sequences(x_test, maxlen = max_len)
-
-
 
 import re
 from tensorflow.keras.layers import Embedding, Dense, LSTM, Bidirectional
@@ -108,9 +87,6 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from sklearn.utils.class_weight import compute_class_weight
 from sklearn.utils.class_weight import compute_class_weight
-
-
-
 
 
 class_weights = compute_class_weight(class_weight = "balanced" , classes=np.unique(y_train),  y=y_train)
@@ -136,17 +112,10 @@ history = model.fit(x_train, y_train, epochs=10, callbacks=[es, mc], batch_size=
 from sklearn.metrics import classification_report
 
 y_pred=model.predict(x_test, batch_size=16, verbose=1)
-
-
 y_pred_ = np.asarray(y_pred)
-
-
-
 y_pred_.argmax(axis=1)
-
-
-
 print(classification_report(y_test, y_pred_.argmax(axis=1), digits=4))
+
 
 # CNN MODEL
 
@@ -178,12 +147,9 @@ batch_size = 32
 
 history = model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, validation_split=0.1, class_weight=class_weights)
 
-
-
 from sklearn.metrics import classification_report
 
 y_pred=model.predict(x_test, batch_size=32, verbose=1)
-
 
 y_pred_ = np.asarray(y_pred)
 print(classification_report(y_test, y_pred_.argmax(axis=1),digits=4))
